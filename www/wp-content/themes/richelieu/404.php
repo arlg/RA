3734c894703ed2552629b799a96299446e9f6a6a
AurelienG <gantier.aurelien@gmail.com>
2016-10-22 17:31:33 +0200

    404

---
+++ www/wp-content/themes/richelieu/404.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * The main template file
+ *
+ *
+ * @link http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Richelieu
+ */
+
+get_header(); ?>
+
+
+
+<section id="blog" class="main_section">
+	
+	<div class="grid_container">
+		
+		<div class="row">
+			<div class="col_1"></div>
+			<div class="col_10">
+				<div id="blog_contents">
+					
+					<h2>Error 404 : This page cannot be found</h2>
+
+				</div>
+			</div>
+			<div class="col_1"></div>
+		</div>
+	</div>
+</section>
+
+
+<?php get_footer(); ?>
